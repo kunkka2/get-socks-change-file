d8b601ad141d89c043e9632fc8b4bb6033905149
softtiny <yingxiancapatin@gmail.com>
2025-05-21 13:22:40 +0800

    :construction: Work in progress.

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,14 @@ use serde::{Serialize, Deserialize};
 use tokio::runtime;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Config {
+struct ProxySource {
     socks_list_url: String,
     get_list_proxy: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Config {
+    proxy_sources: Vec<ProxySource>,
     cert_path: String,
 }
 fn main() {
@@ -30,14 +35,16 @@ fn main() {
 
 async fn get_socks_list(config: &Config) -> Result<String,Box<dyn std::error::Error>> {
     let mut builder = reqwest::Client::builder();
-    if  !config.get_list_proxy.is_none() {
-        let get_list_proxy = config.get_list_proxy.as_ref().unwrap();
+    let oneproxy = config.proxy_sources.first().unwrap();
+
+    if  !oneproxy.get_list_proxy.is_none() {
+        let get_list_proxy = oneproxy.get_list_proxy.as_ref().unwrap();
         let proxy = Proxy::all(get_list_proxy).expect("Could not get all proxies");
         builder = builder.proxy(proxy);
     }
     builder = builder.timeout(Duration::from_secs(3));
     let client = builder.build().expect("Could not connect to server");
-    let data = client.get(&config.socks_list_url).send().await.expect("Could not get socks list");
+    let data = client.get(&oneproxy.socks_list_url).send().await.expect("Could not get socks list");
     let text = data.text().await.expect("Could not read content");
     Ok(text)
 }
