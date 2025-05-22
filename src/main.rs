5e04b066fb416c63fcae20ca17c83fd62697aad4
softtiny <yingxiancapatin@gmail.com>
2025-05-22 15:16:02 +0800

    :construction: Work in progress.

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use std::fs::File;
 use std::time::Duration;
 use reqwest::Proxy;
 use serde::{Serialize, Deserialize};
-use tokio::runtime;
+use tokio::{runtime, task};
+use futures::future::join_all;
 
-#[derive(Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, PartialEq, Clone, Serialize, Deserialize)]
 struct ProxySource {
     socks_list_url: String,
     get_list_proxy: Option<String>,
@@ -26,19 +27,30 @@ fn main() {
         .build()
         .unwrap()
         .block_on(async move {
+            let mut handles =Vec::new();
             for oneproxy in &config.proxy_sources {
+                let handle = task::spawn(get_socks_list(oneproxy.clone()));
+                handles.push(handle);
+                //let handle = task.spawn(get_socks_list((oneproxy)));
 
-                let res = get_socks_list(oneproxy).await;
-                if res.is_ok() {
-                    print!("{}",res.unwrap());
+                // let res = get_socks_list(oneproxy).await;
+                // if res.is_ok() {
+                //     print!("{}",res.unwrap());
+                // }
+            }
+            let all_res = join_all(handles).await;
+            for res in all_res {
+                if res.is_ok(){
+                    let txt = res.unwrap();
+                    println!("{}",txt);
                 }
             }
+            println!("still use conifg {}",config.proxy_sources.len());
         })
 }
 
-async fn get_socks_list(oneproxy: &ProxySource) -> Result<String,Box<dyn std::error::Error>> {
+async fn get_socks_list(oneproxy: ProxySource) -> String {
     let mut builder = reqwest::Client::builder();
-
     if  !oneproxy.get_list_proxy.is_none() {
         let get_list_proxy = oneproxy.get_list_proxy.as_ref().unwrap();
         let proxy = Proxy::all(get_list_proxy).expect("Could not get all proxies");
@@ -48,5 +60,5 @@ async fn get_socks_list(oneproxy: &ProxySource) -> Result<String,Box<dyn std::er
     let client = builder.build().expect("Could not connect to server");
     let data = client.get(&oneproxy.socks_list_url).send().await.expect("Could not get socks list");
     let text = data.text().await.expect("Could not read content");
-    Ok(text)
+    text
 }
